ec5308a6bdff5ba8cec9cdd830520479a9d7b42b
Mateusz Gąsior <mr.nimelo@gmail.com>
2015-04-22 16:54:12 +0200

    [Added second camera][OnTheFly]

--- GK/Assets/Scripts/CameraController.cs
+++ GK/Assets/Scripts/CameraController.cs
@@ -4,20 +4,18 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
-	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - player.transform.position;
 	}
+
 	void Update(){
-		if (Input.GetMouseButton(0)) {
-			transform.LookAt(transform);
-			transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X")*0.2f);
-		}
+
 	}
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = player.transform.position + offset;
+		transform.position = player.transform.position;
+		this.transform.position = this.transform.position + new Vector3(0,10f,0);
+
 	}
 }
---
+++ GK/Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCamera : MonoBehaviour {
+	public Camera MainCamera;
+	public Camera PlayerCamera;
+
+	// Use this for initialization
+	void Start () {
+		this.MainCamera.enabled = false;
+		this.PlayerCamera.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Tab))
+		{
+			this.MainCamera.enabled = !this.MainCamera.enabled; 
+			this.PlayerCamera.enabled = !this.PlayerCamera.enabled;
+		}
+	
+	}
+}
